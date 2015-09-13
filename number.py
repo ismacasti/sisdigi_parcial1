1f378fff4ca0fa4113743bfac00743b0e5476187
Ismael Castiñeira <ismael.castineira@edu.turkuamk.fi>
2015-09-13 17:14:13 -0500

    proper error handling and bugs out

--- number.py
+++ number.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
+class BaseError(Exception):
+    def __init__(self, number, failed_char, base):
+        self.value = [number, failed_char, base]
+    
+    def __str__(self):
+        return repr(self.value)
+
 
 class Number:
     """Class that represents a number in an arbitrary base"""
     
-    class BaseError(Exception):
-        def __init__(self, number, failed_char, base):
-            self.value = [number, failed_char, base]
-        
-        def __str__(self):
-            return repr(self.value)
-
     def insert_number(self, number, base):
-        verify_number(number, base)
+        self.verify_number(number, base)
         self.number_string = number
         self.number_base = base
         
@@ -21,20 +21,23 @@ class Number:
         self.number_base = 0
         
     def __init__(self, number_string, number_base):
-        
+        self.insert_number(number_string, number_base)
         
     def get_in_base(self, base):
         print("heheheh")
         
     def verify_number(self, number, base):
         for character in number:
-            n = char_to_digit(character)
+            n = self.char_to_digit(character)
             if n >= base:
-                raise BaseError(number, n, base)
+                raise BaseError(number, character, base)
     
-    def char_to_digit(char):
+    def char_to_digit(self, char):
         if char.isdigit():
             return int(ord(char)-ord("0"))
         else:
             return int(ord(char) + 10 - ord("a"))
+        
+    def __str__(self):
+        return "({})_{}".format(self.number_string, str(self.number_base))
             
